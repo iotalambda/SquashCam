ffb2c095fcadc280efd208de0e8f850ed78683af
Joona Luoma <joona.luoma@hotmail.com>
2020-12-30 02:22:26 +0200

    Load image to opencv

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.clock import Clock
 from PIL import Image
+import cv2 as cv
+import numpy
 
 # from kivy.logger import Logger
 # import logging
@@ -17,15 +19,16 @@ class CamScreen(BoxLayout):
         camera = self.ids["camera"]
         data = camera.texture.pixels
         size = camera.texture.size
-        image = Image.frombytes(mode="RGBA", size=size, data=data)
-
-        pass
+        pil_image = Image.frombytes(mode="RGBA", size=size, data=data)
+        cv_image = cv.cvtColor(numpy.array(pil_image.convert("RGB")), cv.COLOR_RGB2BGR)
+        cv.imshow("HEIPPA", cv_image)
 
 
 class SquashCamApp(App):
     def build(self):
         camScreen = CamScreen()
         Clock.schedule_interval(camScreen.update, 1.0 / 10)
+        # Clock.schedule_interval(camScreen.update, 10)
         return camScreen
 
 
